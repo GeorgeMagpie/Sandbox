1fab825a203a30e0df62856e09ce46b923a159db
George <prich845@gmail.com>
2022-09-16 07:26:17 +0300

    Теперь эта породия на калькулятор не сдохнет от первого чиха, но все еще можно чихнуть дважды. Вместо операторов все еще можно добавить что угодно и программа будет выдавать результат. Пока что нипонятно что происходит.

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,16 +1,21 @@
 fun main() {
+    val z: Double
     println("Привет юзвер")
     println("Введите первое число")
-    val x = readln().toInt()
+    val x = readln().toDoubleOrNull()
     println("Введите второе число")
-    val y = readln().toInt()
+    val y = readln().toDoubleOrNull()
     println("Выберете оператор: +, -, * или /")
     val i = readln()
-    val z = calc(x, y, i)
-    println(" Ваш результат $z")
+    if (x == null || y == null || i.isEmpty()){
+        println("Введите корректные данные")
+    } else {
+         z = calc(x, y, i)
+        println(" Ваш результат $z")
+    }
 }
 
- fun calc(x: Int, y: Int, i: String): Int{
+ fun calc(x: Double, y: Double, i: String): Double{
      val z = when(i){
          "+" -> x + y
          "-" -> x - y

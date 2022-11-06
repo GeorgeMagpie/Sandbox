4953e997dcdd75bff6b41d3fce672ec73e1dd85b
George <prich845@gmail.com>
2022-11-06 13:00:20 +0300

    Я, конечно все сделал, но не сказать, чтобы все понял. Вопрос остается прежним: Зачем это использовать?? Для чего?? И как??

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,19 +1,17 @@
 fun main() {
-    runSimulation("Guyal", ::printConstructionCost) { playerName, numBuildings ->
+    runSimulation()
+}
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return { playerName: String ->
         val currentYear = 2018
-        println("Adding $numBuildings houses")
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-
-}
-
-inline fun runSimulation(playerName: String,
-                         costPrinter: (Int) -> Unit,
-                         greetingFunction: (String, Int) -> String){
-    val xx  = (1..3).random()
-    println(greetingFunction(playerName, xx))
-}
-fun printConstructionCost(numBuildings: Int) {
-    val cost = 500
-    println("construction cost: ${cost * numBuildings}")
 }
